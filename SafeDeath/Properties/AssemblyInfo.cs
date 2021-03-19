c39c9a1e194735a6a3b36e3bea2c67f46b72dcd9
Julian <julian.declercq1@gmail.com>
2021-03-19 22:35:12 +0100

    * Created SafeDeath mod.

---
+++ SafeDeath/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SafeDeath")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SafeDeath")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("55a5b305-c345-4d4e-a1bb-39e4643fffef")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SafeDeath/SafeDeath.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafeDeath
+{
+    [BepInProcess("valheim.exe")]
+    [BepInPlugin("juliandeclercq.SafeDeath", "Safe Death", "1.0.0")]
+    public class SafeDeath : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
+        }
+
+        [HarmonyPatch(typeof(Player), "CreateTombStone")]
+        static class CreateTombStonePatch
+        {
+            static bool Prefix()
+            {
+                return false; // stop executing prefixes and skip the original
+            }
+        }
+
+        [HarmonyPatch(typeof(Player), "HardDeath")]
+        static class HardDeathPatch
+        {
+            static bool Prefix(ref bool __result)
+            {
+                __result = false;
+                return false; // stop executing prefixes and skip the original
+            }
+        }
+    }
+}
