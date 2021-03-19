9a2ef2b59f4990972ecd486a667c9952dac8e912
Julian <julian.declercq1@gmail.com>
2021-03-19 16:08:43 +0100

    * Added unique save files per character per world * Fixed not being able to unlock map for multiple worlds in same session

--- ExploreMap/ExploreMap.cs
+++ ExploreMap/ExploreMap.cs
@@ -1,18 +1,17 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
 using HarmonyLib;
-using System;
 using System.IO;
 using System.Reflection;
-using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 namespace ExploreMap
 {
     [BepInProcess("valheim.exe")]
-    [BepInPlugin("juliandeclercq.ExploreMap", "Explore Map", "1.0.0.1")]
+    [BepInPlugin("juliandeclercq.ExploreMap", "Explore Map", "1.0.0.0")]
     public class ExploreMap : BaseUnityPlugin
     {
+        private static string _cachedPath = string.Empty;
         private static ConfigEntry<bool> _loadOriginalMap;
         private void Awake()
         {
@@ -24,59 +23,63 @@ namespace ExploreMap
         [HarmonyPatch(typeof(Minimap), "UpdateMap")]
         static class UpdateMapPatch
         {
-            private static bool once = false;
-            private static string cachedPath = string.Empty;
 
             static void Postfix(Minimap __instance)
             {
-                // avoid actually running this every update
-                if (once)
-                    return;
-
-                once = true;
-
                 // generate the path for the saved map
-                if (string.IsNullOrEmpty(cachedPath))
+                if (string.IsNullOrEmpty(_cachedPath))
                 {
-                    string fileName = "originallyExplored.dat";
+                    var world = Traverse.Create(WorldGenerator.instance).Field("m_world").GetValue() as World;
+                    string fileName = $"{Game.instance.GetPlayerProfile().GetFilename()}_{world.m_uid}_ExploreMapSave.dat";
                     string currentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
                     string targetPath = Path.Combine(currentDirectory, @"..\cache\");
-                    cachedPath =  Path.Combine(targetPath, fileName);
+
+                    // create the final path
+                    _cachedPath = Path.Combine(targetPath, fileName);
+                    Debug.LogError($"cachedPath set to {_cachedPath}");
                 }
+
+                // if a saved map exists, the map has been fully explored before
+                if (File.Exists(_cachedPath) && !_loadOriginalMap.Value)
+                    return;
 
                 // load the original map if specified in config
                 if (_loadOriginalMap.Value)
                 {
-                    if (!File.Exists(cachedPath))
+                    if (!File.Exists(_cachedPath))
                     {
-                        Debug.LogWarning($"Couldn't load saved map from file {cachedPath}, file doesn't exist.");
+                        Debug.LogWarning($"Couldn't load saved map from file {_cachedPath}, file doesn't exist.");
                         return;
                     }
-                    var data = File.ReadAllBytes(cachedPath);
-                    Traverse.Create(__instance).Method("SetMapData", data).GetValue(); // getvalue is to ensure the method gets invoked afaik
+                    var data = File.ReadAllBytes(_cachedPath);
+                    Traverse.Create(__instance).Method("SetMapData", data).GetValue(); // getvalue is to ensure the method gets invoked
 
                     // delete the saved map
-                    File.Delete(cachedPath);
+                    File.Delete(_cachedPath);
                     return;
                 }
-
-                // if a saved map exists, the map has been fully explored before
-                if (File.Exists(cachedPath))
-                    return;
 
                 // save the map data, then retrieve it (easiest way to retrieve current map data)
                 __instance.SaveMapData();
                 byte[] mapData = Game.instance.GetPlayerProfile().GetMapData();
 
                 // write the map to a file
-                File.WriteAllBytes(cachedPath, mapData);
-                UnityEngine.Debug.Log($"wrote mapdata to {cachedPath}");
+                File.WriteAllBytes(_cachedPath, mapData);
+                Debug.Log($"wrote mapdata to {_cachedPath}");
 
                 // explore the whole map
                 __instance.ExploreAll();
             }
         }
-    }
 
-    
+        [HarmonyPatch(typeof(ZNet), "OnDestroy")]
+        static class OnDestroyPatch
+        {
+            static void Postfix(Minimap __instance)
+            {
+                _cachedPath = string.Empty;
+            }
+        }
+    }
 }
