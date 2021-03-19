637318140748a64e0af79ac87da6452494fb174e
Julian <julian.declercq1@gmail.com>
2021-03-19 01:25:53 +0100

    * Added ExploreMap project and basic functionality

---
+++ ExploreMap/ExploreMap.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+
+namespace ExploreMap
+{
+    [BepInProcess("valheim.exe")]
+    [BepInPlugin("juliandeclercq.ExploreMap", "Explore Map", "1.0.0.0")]
+    public class ExploreMap : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
+        }
+
+        [HarmonyPatch(typeof(Minimap), "UpdateMap")]
+        static class UpdateMapPatch
+        {
+            private static bool _explored = false;
+            static void Postfix(Minimap __instance)
+            {
+                if (_explored)
+                    return;
+                
+                __instance.ExploreAll();
+                _explored = true;
+                UnityEngine.Debug.Log("Explored whole minimap!"); // minimap <-> map??
+            }
+        }
+    }
+}
