cec4b51e33cbede3ca7ffd72ecc62e6bdfb8a635
Julian <julian.declercq1@gmail.com>
2021-03-21 20:26:21 +0100

    * SafeDeath is now compatible with More Slots mod.

--- SafeDeath/SafeDeath.cs
+++ SafeDeath/SafeDeath.cs
@@ -11,7 +11,7 @@ using UnityEngine;
 namespace SafeDeath
 {
     [BepInProcess("valheim.exe")]
-    [BepInPlugin("juliandeclercq.SafeDeath", "Safe Death", "1.0.0")]
+    [BepInPlugin("juliandeclercq.SafeDeath", "Safe Death", "1.1.0")]
     public class SafeDeath : BaseUnityPlugin
     {
         private void Awake()
@@ -22,9 +22,20 @@ namespace SafeDeath
         [HarmonyPatch(typeof(Player), "CreateTombStone")]
         static class CreateTombStonePatch
         {
-            static bool Prefix()
+            static bool Prefix(Player __instance, out Inventory __state)
             {
+                var invent = __instance.GetInventory();
+                
+                Inventory savedInventory = new Inventory("SavedInventory", null, invent.GetWidth(), invent.GetHeight());
+                savedInventory.MoveAll(invent);
+                __state = savedInventory;
                 return false; // stop executing prefixes and skip the original
+            }
+
+            static void Postfix(Player __instance, Inventory __state)
+            {
+                var invent = __instance.GetInventory();
+                invent.MoveAll(__state);
             }
         }
 
