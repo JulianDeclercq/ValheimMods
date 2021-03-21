aa7d699ef08a13c9a8d7f9687f22d2bc0caec979
Julian <julian.declercq1@gmail.com>
2021-03-21 20:07:28 +0100

    * Food is now saved upon death

--- SafeDeath/SafeDeath.cs
+++ SafeDeath/SafeDeath.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace SafeDeath
 {
@@ -34,6 +35,20 @@ namespace SafeDeath
             {
                 __result = false;
                 return false; // stop executing prefixes and skip the original
+            }
+        }
+
+        [HarmonyPatch(typeof(Player), "OnDeath")]
+        static class OnDeathPatch
+        {
+            static void Prefix(List<Player.Food> ___m_foods, out List<Player.Food> __state)
+            {
+                __state = new List<Player.Food>(___m_foods);
+            }
+
+            static void Postfix(ref List<Player.Food> ___m_foods, List<Player.Food> __state)
+            {
+                ___m_foods = __state;
             }
         }
     }
